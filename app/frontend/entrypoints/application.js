69210254ded74843f1e3872452224f2831a14a1f
Yo Kamada <bumpfuji10@gmail.com>
2025-05-24 17:27:58 +0900

    [add]font-awesome install

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,7 +1,15 @@
 import { createApp } from 'vue'
 import Header from '../components/Header.vue'
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUser, faSignInAlt, faPlusCircle } from '@fortawesome/free-solid-svg-icons'; // 使いたいアイコンをインポートだもの
+library.add(faUser, faSignInAlt, faPlusCircle);
 
 document.addEventListener('DOMContentLoaded', () => {
   const headerEl = document.getElementById('header')
-  if (headerEl) createApp(Header).mount(headerEl)
+  if (headerEl) {
+    const app = createApp(Header)
+    app.component('font-awesome-icon', FontAwesomeIcon);
+    app.mount(headerEl);
+  }
 })
