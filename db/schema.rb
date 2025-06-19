b156dc703673b710ed2b7d77cc2592a5ae155b91
bumpfuji10 <bumpfuji10@gmail.com>
2025-06-19 03:56:18 +0000

    [add]guest user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_17_085518) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_19_035200) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "guest_users", force: :cascade do |t|
+    t.string "session_token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_token"], name: "index_guest_users_on_session_token", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
