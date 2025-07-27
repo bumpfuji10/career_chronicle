3039dec882bc9b20635207198984c493b831d34b
bumpfuji10 <bumpfuji10@gmail.com>
2025-07-27 22:52:40 +0900

    [add]schemaの状況を最新にdumpする

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_17_085518) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_26_074149) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "resumes", force: :cascade do |t|
+    t.string "company"
+    t.string "position"
+    t.text "tasks"
+    t.text "improvements"
+    t.text "achievements"
+    t.text "summary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
