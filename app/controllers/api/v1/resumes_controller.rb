9aa1292bacf9053e88008d35465912aec1e22990
bumpfuji10 <bumpfuji10@gmail.com>
2025-08-09 19:47:30 +0900

    [change]resumes作成用のwebAPIをAPI層に切り出し

--- app/controllers/api/v1/resumes_controller.rb
+++ app/controllers/api/v1/resumes_controller.rb
@@ -1,7 +1,27 @@
 module Api
   module V1
     class ResumesController < ApplicationController
-      
+      before_action :find_or_create_guest_user
+
+      def create
+        resume = @guest_user.resumes.build(resume_params)
+        resume.summary = resume.generate_summary
+        if resume.save
+          render json: { id: resume.id, summary: resume.summary }, status: :created
+        else
+          render json: { errors: resume.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def find_or_create_guest_user
+        @guest_user = ProvideGuestUser.new(session).call
+      end
+
+      def resumes_params
+        params.require(:resume).permit(:company, :position, :tasks, :improvements, :achievements)
+      end
     end
   end
 end
