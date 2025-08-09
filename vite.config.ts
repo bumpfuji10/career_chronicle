d5b1e128e2a9aa20228e1a732a18530d145a7236
bumpfuji10 <bumpfuji10@gmail.com>
2025-08-09 21:18:43 +0900

    [change]開発環境でviteが適切に動くように、npmをinstallするようにdocker composeを修正

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,13 @@ export default defineConfig({
       }
     })
   ],
+  server: {
+    host: '0.0.0.0',
+    port: 3036,
+    strictPort: true,
+    hmr: {
+      host: 'localhost',
+      port: 3036
+    }
+  }
 })
