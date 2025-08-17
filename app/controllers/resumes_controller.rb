a0b1f33c504247d0ed2748a6bd67f7c36ed2dc67
bumpfuji10 <bumpfuji10@gmail.com>
2025-08-17 14:26:29 +0000

    詳細画面を実装

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,8 +1,16 @@
 class ResumesController < ApplicationController
   before_action :set_user
   before_action :check_guest_resume_limit, only: [:new]
+  before_action :set_resume, only: [:show]
 
   def new; end
+
+  def show
+    # ゲストユーザーは自分のresumeのみ閲覧可能
+    if @user.is_a?(Guest) && @resume.user_id != @user.id
+      redirect_to root_path, alert: "アクセス権限がありません"
+    end
+  end
 
   private
 
@@ -14,5 +22,9 @@ class ResumesController < ApplicationController
     if @user.is_a?(Guest) && @user.resumes.exists?
       redirect_to "/", alert: "ゲストユーザーは職務経歴書を1件までしか作成できません。アカウントの登録もしくはログインをしていただくことで2件目の作成が可能となっております。"
     end
+  end
+
+  def set_resume
+    @resume = Resume.find(params[:id])
   end
 end
