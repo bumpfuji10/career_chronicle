896c12baf12c15961b618def3e1f1c07f03d3c7b
bumpfuji10 <bumpfuji10@gmail.com>
2025-10-12 01:28:32 +0000

    [add]achievements

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
       resources :companies, only: [:create, :update]
       resources :positions, only: [:create, :update]
       resources :tasks, only: [:create, :update]
+      resources :achievements, only: [:create, :update]
     end
   end
 end
