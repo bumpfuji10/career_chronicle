9980d292000bd7e920afff36b31effb72daec4d2
bumpfuji10 <bumpfuji10@gmail.com>
2025-10-13 08:04:29 +0000

    [add]resumesのfactoryにsummaryを付与したtraitを追加

--- spec/factories/resumes.rb
+++ spec/factories/resumes.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :resume do
     association :user
+    summary { nil }
+
+    trait :with_summary do
+      summary { '私は株式会社ABCで営業として働きました。その中で顧客との関係構築に注力し、売上目標を達成しました。' }
+    end
   end
 end
